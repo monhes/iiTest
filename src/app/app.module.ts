8c9b309a770f9b806406df5ad3e535f3e47dd86a
thud aneknual <monhomes@hotmail.com>
2022-10-21 18:24:44 +0700

    init component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserLoginComponent } from './login/user-login/user-login.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
+import { UserRegisterComponent } from './register/user-register/user-register.component';
+import { UserEditProfileComponent } from './editProfile/user-edit-profile/user-edit-profile.component';
+import { UserProfileComponent } from './userProfile/user-profile/user-profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserLoginComponent,
+    UserRegisterComponent,
+    UserEditProfileComponent,
+    UserProfileComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot([
+      {path: 'login' , component:UserLoginComponent},
+      {path: 'register' , component:UserRegisterComponent},
+      {path: 'editprofile' , component:UserEditProfileComponent},
+      {path: 'userprofile' , component:UserProfileComponent},
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
